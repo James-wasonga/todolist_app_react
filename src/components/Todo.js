dd1363fac24dd28c51d6e5e6ad8056ba74cc94cb
James-wasonga <jameswasonga22@gmail.com>
2024-05-23 18:48:04 +0300

    react to do list

---
+++ src/components/Todo.js
@@ -0,0 +1,20 @@
+import { faPenToSquare } from '@fortawesome/free-regular-svg-icons'
+import { faTrash } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import React from 'react'
+
+const Todo = ({task, toggleComplete, deleteTodo, EditTodo}) => {
+  return (
+    <div className='Todo'>
+        <p onClick={() => toggleComplete(task.id)}  className={`${task.completed ? 'completed': ""}`}>{task.task}</p>
+    <div>
+        <FontAwesomeIcon icon={faPenToSquare} onClick={() => EditTodo(task.id)}/>
+        <FontAwesomeIcon icon={faTrash} onClick={() => deleteTodo(task.id)}/>
+
+    </div>
+        
+    </div>
+  )
+}
+
+export default Todo
